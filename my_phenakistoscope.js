6fcdcfdf8f83ab39dd3e2449565f11e3d678a5b7
Daws18 <dawsonfernandes18@gmail.com>
2023-08-17 18:01:01 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,35 +1,35 @@
 const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 237, 231, 194);  //lets us draw the whole circle background, ignoring the boundaries
 
   var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
+  layer1.mode( SWIRL(4) );
   layer1.set_boundary( 200, 1000 );
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  //var layer2 = new PLayer(squares);
+  //layer2.mode( RING );
+  //layer2.set_boundary( 0, 400 );
 }
 
 function faces(x, y, animation, pScope){
   
-  scale(animation.frame*2);
+  scale(animation.wave(4));
 
+  fill(191, 19, 13);
   ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  //ellipse(-10,-10,10,10); //draw eye
+  //ellipse(10,-10,10,10); // draw eye
+  //arc(0,10,20,10,0,180); // draw mouth
 
 }
 
@@ -47,4 +47,3 @@ function squares(x, y, animation, pScope){
   rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
 
 }
-//test
