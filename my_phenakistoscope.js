98aaf5621df52b07db8ab8ab1bf5585064096707
Daws18 <dawsonfernandes18@gmail.com>
2023-09-14 12:41:12 +1200

    cleaning code

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -10,39 +10,43 @@ function setup_pScope(pScope){
   pScope.load_image_sequence("moving_dragon", "png", 11);
   pScope.load_image("symbol","png")
   pScope.load_image("cracker", "png")
+  pScope.load_image("lantern", "png")
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 237, 231, 194);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null,237, 231, 194);  //lets us draw the whole circle background, ignoring the boundaries
 
-  let insideBackground = new PLayer(insideRed);
+  let insideBackground = new PLayer(insideRed);//red centre ring 
   insideBackground.mode(RING);
   insideBackground.set_boundary(0,100);
 
-  let outsideBackground = new PLayer(outsideRed);
+  let outsideBackground = new PLayer(outsideRed);//red boundary ring 
   outsideBackground.mode(RING);
   outsideBackground.set_boundary(970,1000);
 
-  var layer1 = new PLayer(firework);
+  var layer1 = new PLayer(firework);//red and yellow ellipses  
   layer1.mode( SWIRL(4) );
   layer1.set_boundary( 100, 800 );
 
-  var layer2 = new PLayer(cracker);
+  var layer2 = new PLayer(cracker);//firecracker asset 
   layer2.mode( SWIRL(1) );
   layer2.set_boundary( 100, 300 );
 
-  let dragonSequence = new PLayer(dragon);
+  var layer3 = new PLayer(lantern);//lantern asset 
+  layer3.mode( SWIRL(1) );
+  layer3.set_boundary( 800, 1000 );
+
+  let dragonSequence = new PLayer(dragon);//dragon sequence
   dragonSequence.mode(RING);
   dragonSequence.set_boundary(0,800);
 
-  let CenterImage = new PLayer(symbol);
+  let CenterImage = new PLayer(symbol);//dragon symbol asset 
   CenterImage.mode(RING);
   CenterImage.set_boundary(0,30);
   
 
-  }
-
+}
 function symbol(x,y,animation,pScope){
   scale(0.8);
   pScope.draw_image("symbol",x,y);//dragon charcter symbol text type 
@@ -56,32 +60,14 @@ function firework(x, y, animation, pScope){
   scale(animation.wave(4));
 
   fill(191, 19, 13);//red 
-  ellipse(0,0,60,60); // red circle 
+  ellipse(0,0,60,60);//red circle 
   
 
   fill(237, 161, 9)//yellow 
   let ballSize  = 20 + (animation.wave(1)* 20)
-  ellipse(0, 0,ballSize); 
+  ellipse(0,0,ballSize); 
   
-
 }
-
-//function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  //let angleOffset = (360 / SLICE_COUNT) / 2
-  //let backgroundArcStart = 270 - angleOffset;
-  //let backgroundArcEnd = 270 + angleOffset;
-
-  //fill(66, 135, 245)
-  //arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-  //scale(animation.wave(1));
-  //fill(255)
-  //ellipse(0,0,50,50);
-  //rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
-
-
 function insideRed (x,y,animation,pScope){
   pScope.fill_background(191, 19, 13, 255);//red 
 }
@@ -89,8 +75,10 @@ function outsideRed (x,y,animation,pScope){
   pScope.fill_background(191, 19, 13, 255);//red 
 }
 function dragon(x,y,animation,pScope){
-  // translate(x,y-650);
-  scale(0.5);
-  //pScope.load_image("moving_dragon" , "png");
-  pScope.draw_image_from_sequence("moving_dragon", 0, -700, animation.frame);
+  scale(0.45);//size of image
+  pScope.draw_image_from_sequence("moving_dragon", 0, -700, animation.frame); //animated dragon sequence
+}
+function lantern(x,y,animation,pScope){
+  scale(0.4);
+  pScope.draw_image("lantern",x,y);// lantern asset
 }
